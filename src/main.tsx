57363f7a28837860f52badbaa03fe8ea452114f4
raethania <larasatipmaharani@gmail.com>
2026-05-27 05:34:39 +0700

    chore: configure frontend project setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './index.css'
+import AppRoutes from './routes/AppRoutes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      {/* Routes */}
-      <Routes>
-
-      </Routes>
-    </BrowserRouter>
+        <AppRoutes/>
+    </BrowserRouter>  
   </StrictMode>,
 )
